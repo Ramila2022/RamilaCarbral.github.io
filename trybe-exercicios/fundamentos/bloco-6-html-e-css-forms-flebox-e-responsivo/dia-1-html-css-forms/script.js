b34e8b2e0e05d8cc882a966171494715a80b925a
ramila <ramilac2006@hotmail.com>
2022-07-07 14:35:49 -0300

    forms pronto, faltando css

---
+++ trybe-exercicios/fundamentos/bloco-6-html-e-css-forms-flebox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,11 @@
+// const button = document.querySelector('#button-submit');
+// const forms = document.querySelector('#form');
+// const form = document.querySelector('#userName');
+
+
+// function getUserValue(envent){
+//     envent.preventdefault();
+//     console.log(form.value);
+//     form.innerHTML = form.value
+// }
+// button.addEventListener('click', getUserValue);
